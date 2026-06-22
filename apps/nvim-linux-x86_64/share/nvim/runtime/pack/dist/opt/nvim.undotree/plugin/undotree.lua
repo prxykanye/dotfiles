44a38b12d9e8c2a3271bd07502cdc7701a722517
prxykanye <115326081+prxykanye@users.noreply.github.com>
2026-06-22 23:48:47 +0700

    Delete apps/nvim-linux-x86_64 directory

--- apps/nvim-linux-x86_64/share/nvim/runtime/pack/dist/opt/nvim.undotree/plugin/undotree.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.loaded_undotree_plugin ~= nil then
-  return
-end
-vim.g.loaded_undotree_plugin = true
-
-vim.api.nvim_create_user_command('Undotree', function()
-  require 'undotree'.open()
-end, {})
